9737f2826c55fbf2a61330578de5d01e5febb4e3
Valiveti Sai Mahitha <Valivetisaimahitha@gmail.com>
2026-08-30 06:35:37 +0530

    Time: 3 ms (61.82%) | Memory: 87.1 MB (8.73%) - LeetSync

---
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+
+        int n = nums.length;
+
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        // Find min and max indices
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] < nums[minIndex]) {
+                minIndex = i;
+            }
+
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        // Put smaller index in minIndex
+        int left = Math.min(minIndex, maxIndex);
+        int right = Math.max(minIndex, maxIndex);
+
+        // 1. Remove both from the front
+        int fromFront = right + 1;
+
+        // 2. Remove both from the back
+        int fromBack = n - left;
+
+        // 3. Remove one from front and one from back
+        int fromBoth = (left + 1) + (n - right);
+
+        return Math.min(fromFront, Math.min(fromBack, fromBoth));
+    }
+}
